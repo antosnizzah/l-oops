6249c910a80d05ab45da1cf0cf3f384e18715dba
antosnizzah <antonygatitu327@gmail.com>
2024-10-28 09:14:23 +0300

    js:loops ie for,do while and while

---
+++ script.js
@@ -0,0 +1,30 @@
+// loops are used to repeat a block of code multiple times.
+// syntax of for loop
+
+// for(initialization; condition; increment/decrement){
+//     code to be executed
+// }
+
+for (i=0; i<=10; i++){
+    console.log("today is on monday");
+}
+
+
+// simple for loop
+
+// let x = 2
+
+// for(x=2;x<=10;x++){
+//     console.log("the value of x is:"," "+x)
+// }
+
+// function countNumbers(){
+//     const output = document.getElementById("countOutputs");
+//     output.innerHTML = " ";
+//     let result = " ";
+
+//     for (let i = 0; i < 10; i++) {
+//         result += i + " ";
+//     }
+//     output.textContent= `counting: ${result}`
+// }
