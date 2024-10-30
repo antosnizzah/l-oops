7822f471a63720837438441560e3556bbd48b2fe
antosnizzah <antonygatitu327@gmail.com>
2024-10-30 15:03:32 +0300

    updated the js

--- script.js
+++ script.js
@@ -1,30 +1,82 @@
-// loops are used to repeat a block of code multiple times.
-// syntax of for loop
-
-// for(initialization; condition; increment/decrement){
-//     code to be executed
-// }
-
-for (i=0; i<=10; i++){
-    console.log("today is on monday");
-}
 
 
-// simple for loop
 
-// let x = 2
 
-// for(x=2;x<=10;x++){
-//     console.log("the value of x is:"," "+x)
-// }
 
-// function countNumbers(){
-//     const output = document.getElementById("countOutputs");
-//     output.innerHTML = " ";
-//     let result = " ";
 
-//     for (let i = 0; i < 10; i++) {
-//         result += i + " ";
+
+
+
+
+
+
+
+// // for loop 
+//  for(i=0; i<=10; i++)[
+//     console.log(i+ " ","today is on wednesday")
+//  ]
+// //  for loop for counting numbers ie 1,2,3,4,5,6,7,8,9,10
+//  function countsNumbers(){
+//     let output = document.getElementById("countOutput");
+//     results =  " ";
+//     for (let i = 0; i<10 ; i++){
+//         results += i + " ";
 //     }
-//     output.textContent= `counting: ${result}`
+//     output.textContent= `counting: ${results}`;
+//  }
+// //  while loop
+//  let j = 0;
+//  while(j<=10){
+//     console.log(j+ " ","today is on wednesday")
+//     j++;
+//  }
+
+// //  do while loop
+// let k = 0;
+// do{
+//    console.log(k+ "","today is on sunday")
+//    k++;
+// } while(k < 10)
+
+   // for of loop
+   // let names = ["John","Jane","Jill","Jack"];
+   // for (let name of names){
+   //    console.log(name + " ","is my friend");
+   // }
+   
+   // let names = ["John", "Jane", "Jill", "Jack"];
+   // let text = "";
+   // let output = document.getElementById("output");
+   
+   // for (let name of names) {
+   //     text += `${name} is my friend<br>`; // Adds each name with "is my friend" and a line break
+   // }
+   
+   // output.innerHTML = text; // Use innerHTML to render the <br> tags
+   
+   // for in loop
+   // let person = {name: "antony",age: "19",originality: "kenyan",year: "2021"};
+   // for (let prop in person){
+   //    console.log(prop+":",""+person[prop]);
+   // }
+//    let person = {
+//       name: "antony",
+//       age: "19",
+//       originality: "kenyan",
+//       year: "2021"
+//   };
+  
+//   let text2 = "";
+//   let output = document.getElementById("output1");
+  
+//   for (let prop in person) {
+//       text2 += `${prop}: ${person[prop]}<br>`; // Build a string with each property and its value
+//   }
+  
+//   output.innerHTML = text2; // Use innerHTML to render the properties in the element
+  
+// infinite while  loop
+//  let x=5=0;
+//  while (x == 5) {
+//    console.log("In the loop");
 // }
